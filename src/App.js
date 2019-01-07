d4c7a230ba3051ec58ebbe86d88e908b00959eff
weiwenl <weiwenl.justin@gmail.com>
2019-01-07 14:46:17 +0800

    user key in url with search term will call api and fill the search field

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import queryString from 'querystring';
 
 //import components
 import UserInput from './UserInput/UserInput';
 import DataOutput from './DataOutput/DataOutput';
 
 class App extends Component {
-  state = {
-    searchQuery: "",
-    repos: []
-  }
+    state = {
+      searchQuery: "",
+      repos: []
+    }
 
-  inputChangeHandler = (event) => {
-    let searchTerm = event.target.value;
-    console.log("search query", event.target.value);
+  componentDidMount() {
+    const values = queryString.parse(this.props.location.search);
+
     this.setState({
-      searchQuery: event.target.value
-    });
+      searchQuery: Object.values(values)
+    })
+    let searchTerm = Object.values(values);
     let url = 'https://api.github.com/search/repositories?q=' + searchTerm;
+
     axios.get(url)
     .then(response => {
       this.setState({
         repos: response.data.items
-      });
-    });
-    event.preventDefault();
+      })
+    }).catch(error => { console.log(error)});
   }
+
+  inputChangeHandler = (event) => {
+    let searchTerm = event.target.value;
+    this.setState({
+      searchQuery: event.target.value
+    });
+
+    let endpoint = 'https://api.github.com/search/repositories?q=' + searchTerm;
+
+    axios.get(endpoint)
+      .then(response => {
+        this.setState({
+          repos: response.data.items
+        })
+      }).catch(error => { console.log(error)});
+      event.preventDefault();
+}
 
 
   render() {
     return (
       <div className="App">
         <h1>Search public Github repositories</h1>
-        <UserInput input={"Type something"} changed={this.inputChangeHandler}/>
+        <UserInput changed={this.inputChangeHandler} value={this.state.searchQuery}/>
         <DataOutput data={this.state.repos}/>
       </div>
     );
